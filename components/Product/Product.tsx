f9a949f7f76ef6c0759482a1d86176f34e19db35
sravshanbek <sravshanbek@it.gov.kg>
2021-08-22 23:17:41 +0600

    next Image compenent

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -7,6 +7,7 @@ import { Tag } from '../Tag/Tag'
 import { Button } from '../Button/Button'
 import { declOfNum, priceRu } from '../../helpers/helpers'
 import { Divider } from '../Divider/Divider'
+import Image from 'next/image'
 
 export const Product = ({product, className, ...props}: ProductProps): JSX.Element => {
   return (
@@ -14,7 +15,12 @@ export const Product = ({product, className, ...props}: ProductProps): JSX.Eleme
       className={styles.product}
     >
       <div className={styles.logo}>
-        <img src={process.env.NEXT_PUBLIC_DOMAIN + product.image} alt={product.title} />
+        <Image 
+          src={process.env.NEXT_PUBLIC_DOMAIN + product.image}
+          alt={product.title}
+          width={70}
+          height={70}
+        />
       </div>
       <div className={styles.title}>{product.title}</div>
       <div className={styles.price}>
