6c363c04ad8fe218ae3c23aed2e70749674494d8
sravshanbek <sravshanbek@it.gov.kg>
2021-09-01 13:07:45 +0600

    github api

--- components/Divider/Divider.tsx
+++ components/Divider/Divider.tsx
@@ -1,6 +1,6 @@
 import { DividerProps } from './Divider.props'
 import cn from 'classnames'
-import styles from './Divider.module.css'
+import styles from './Divider.module.css';
 
 export const Divider = ({className, ...props}: DividerProps): JSX.Element => {
   return (
